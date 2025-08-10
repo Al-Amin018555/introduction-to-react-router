eaa943d2c1742c8e3e51b490d71c11b5c8bd7e1c
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-08-10 14:11:08 +0600

    useNavigation hook added to see loading while data load

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import Header from '../Header/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Footer from '../Footer/Footer';
 
 const Home = () => {
+    const navigation = useNavigation();
     return (
         <div>
             <Header></Header>
             this is the home component
-            <Outlet></Outlet>
+            {
+                navigation.state === "loading" ? 
+                <p>Loading...</p> :
+                <Outlet></Outlet>
+            }
             <Footer></Footer>
         </div>
     );
